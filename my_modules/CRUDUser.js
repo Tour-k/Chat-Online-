9ca8fb2c999d8a5120a058966d7ca056f947f3fd
Manuel Konrath <manuel.konrath@gmail.com>
2020-03-27 00:43:18 +0100

    add my modules

---
+++ my_modules/CRUDUser.js
@@ -0,0 +1,74 @@
+var getUserById = function(conn, id) {
+    conn.query("SELECT * FROM User WHERE id=" + id, function (err, result, fields) {
+        if (err) throw err;
+            return(result);
+        });
+}
+
+// Select All from User : 
+var getAllUsers = function(conn) {
+    var test = 2;
+    // console.log("testCoucou")
+    conn.query("SELECT * FROM User", function (err, result, fields) {
+    if (err) throw err; 
+        return result;
+    });
+}
+
+
+// Select by username from User : 
+var getUserByUsername = function(conn, username) {
+    conn.query("SELECT * FROM User WHERE username = "+ username, 
+    function (err, result, fields) {
+        if (err) throw err;
+        return result;
+    });
+}
+
+//delete:
+var deleteUser = function(conn, id){
+    var sql = "DELETE FROM User WHERE id ="+ id;
+    conn.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Number of records deleted: " + result.affectedRows);
+});
+}
+
+// Insert a new User : 
+var createUser = function(conn, username, password, bio='', avatar=''){
+    var sql = "INSERT INTO User VALUES (NULL," + username + "," + password + "," + bio + ","+ avatar +")";
+    conn.query(sql, function (err, result) {
+    if (err) throw err;
+        console.log( username + "inserted");   
+}); 
+}
+
+//update: 
+var updateUserBio = function (conn, id, bio) {
+    var sql = "UPDATE User SET bio = " + bio + " WHERE id = " + id;
+    conn.query(sql, function (err, result) {
+        if (err) throw err;
+        return result.affectedRows + " record(s) updated";
+});
+}
+
+var updateUserAvatar = function(conn, id, avatar){
+    var sql = "UPDATE User Set avatar ="+avatar+"WHERE id =" + id;
+    conn.query(sql, function (err, result) {
+        if (err) throw err;
+        return result.affectedRows + " record(s) updated";
+    });
+}
+
+exports.getUserById = getUserById;
+exports.getAllUsers = getAllUsers;
+exports.getUserByUsername = getUserByUsername;
+exports.deleteUser = deleteUser;
+exports.createUser = createUser;
+exports.updateUserBio = updateUserBio;
+exports.updateUserAvatar = updateUserAvatar;
+
+
+
+
+
