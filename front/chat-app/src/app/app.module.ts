25c990aa500187f623d4be06f42cbbd987dc00e3
Manuel Konrath <manuel.konrath@gmail.com>
2020-03-27 15:49:35 +0100

    Simple front components and routing

--- front/chat-app/src/app/app.module.ts
+++ front/chat-app/src/app/app.module.ts
@@ -4,11 +4,24 @@ import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AccueilComponent } from './accueil/accueil.component';
+import { LoginWithAccountComponent } from './login-with-account/login-with-account.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { ChannelComponent } from './channel/channel.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+
 const config : SocketIoConfig = {  url: 'http://localhost:8988', options: {} }; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    LoginWithAccountComponent,
+    CreateAccountComponent,
+    ChannelComponent,
+    UserProfileComponent,
+    FourOhFourComponent,
   ],
   imports: [
     BrowserModule,
